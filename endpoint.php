c37e5d41d2a44417bbd82a0b4c43a006cc96565c
julurikarthi <jkarthik31@gmail.com>
2020-08-26 22:22:17 +0530

    Add files via upload
    updating content-type

--- endpoint.php
+++ endpoint.php
@@ -6,17 +6,20 @@ include('DBValues.php');
 if(strcasecmp($_SERVER['REQUEST_METHOD'], 'POST') == 0){
 
 //Make sure that the content type of the POST request has been set to application/json
+		
 		$contentType = isset($_SERVER["CONTENT_TYPE"]) ? trim($_SERVER["CONTENT_TYPE"]) : '';
 
-		if(strcasecmp($contentType, 'application/json') != 0){
-   			 throw new Exception('Content type must be: application/json');
-		}
+		// if(strcasecmp($contentType, 'application/json') != 0){
+  //  			 throw new Exception('Content type must be: application/json');
+		// }
 
 		//Receive the RAW post data.
+		error_log(print_r("content-type ".$contentType, TRUE)); 
 		$content = trim(file_get_contents("php://input"));
 
 		//Attempt to decode the incoming RAW post data from JSON.
 		$decoded = json_decode($content, true);
+		error_log(print_r($decoded, TRUE));
 
 		//If json_decode failed, the JSON is invalid.
 		if(!is_array($decoded)){
